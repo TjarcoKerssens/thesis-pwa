6349fda3f6402345251c7c68f3e3e4b33f75a7ae
Tjarco Kerssens <info@tjarcokerssens.nl>
2019-04-25 10:42:09 +0200

    Initialized the Android version of the application
    This includes:
    * Initializing the app shell
    * Adding two ImageViews
    * Fetching a random image every 5 seconds
    * Altering the image with contrast and inverted colors

---
+++ Android/AndroidPWAThesis/app/src/main/java/com/example/androidpwathesis/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.androidpwathesis
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+const val FETCH_INTERVAL : Long = 5000
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        startFetchingRandomImages()
+    }
+
+    private fun startFetchingRandomImages(){
+        val imageView = findViewById<ImageView>(R.id.randomImageView)
+        val alteredImageView = findViewById<ImageView>(R.id.alterationsImageView)
+        val fetcher = ImageFetcher()
+
+        fetcher.startFetching(imageView, FETCH_INTERVAL)
+        fetcher.setAlterationsView(alteredImageView)
+    }
+}
