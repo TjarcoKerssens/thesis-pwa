a33b43d10b0ceeecd83cc810a7421a562214f450
Tjarco Kerssens <info@tjarcokerssens.nl>
2019-05-17 13:09:43 +0200

    [PWA] updated colors

--- pwa/src/components/Location.js
+++ pwa/src/components/Location.js
@@ -3,6 +3,12 @@ import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
 class Location extends PolymerElement{
     static get template (){
         return html`
+            <style>
+                h2{
+                    font-size: 1em;
+                    margin-bottom: 0;
+                }
+            </style>
             <h2>Current location</h2>
             <span>Longitude: [[longitude]]</span>
             <span>Latitude: [[latitude]]</span>
